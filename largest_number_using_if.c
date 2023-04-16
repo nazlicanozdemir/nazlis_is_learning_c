83003505ca14adf9e0302c9ae577351baf743c43
nazli can ozdemir <130602289+nazlicanozdemir@users.noreply.github.com>
2023-04-16 16:21:19 +0300

    Update largest_number_using_if.c

--- largest_number_using_if.c
+++ largest_number_using_if.c
@@ -11,27 +11,27 @@ int main()
         printf("Please enter 3 integer numbers: ");
         scanf("%d %d %d", &n1, &n2, &n3);
         
-        if (( n1 == n2 ) & (n2 == n3))
+        if (( n1 == n2 ) && (n2 == n3))
         {
             printf("Entered numbers are all equal to each other.\n");
         }
-        else if ( (n1 == n2) & (n1 > n3))
+        else if ( (n1 == n2) && (n1 > n3))
         {
             printf("%d is the largest among entered numbers.\n", n1);
         }
-        else if ( (n1 == n3) & (n1 > n2) )
+        else if ( (n1 == n3) && (n1 > n2) )
         {
             printf("%d is the largest among entered numbers.\n", n1);
         }
-        else if ( (n2 == n3) & (n2 > n1) )
+        else if ( (n2 == n3) && (n2 > n1) )
         {
             printf("%d is the largest among entered numbers.\n", n2);
         }
-        else if ( (n1 > n2) & (n1 > n3) )
+        else if ( (n1 > n2) && (n1 > n3) )
         {
             printf("%d is the largest among entered numbers.\n", n1);
         }
-        else if ( (n2 > n1) & ( n2 > n3) )
+        else if ( (n2 > n1) && ( n2 > n3) )
         {
             printf("%d is the largest among entered numbers.\n", n2);
         }
