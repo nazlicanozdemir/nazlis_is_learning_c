fa423cfd6dcc742115a8f2e9e59a19770b434a96
nazli can ozdemir <130602289+nazlicanozdemir@users.noreply.github.com>
2023-04-16 20:51:19 +0300

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -15,10 +15,10 @@ int main()
         total = total + i;
     }
     printf("The sum of the numbers from 1 to %d is:\n", number);
-    for (i = 1; i < number; i++)
+    for (i = 1; i < number; i++) // i wrote "i < number" instead of "i <= number" this time to avoid sth like "1+2+3+=6"
     {
         printf("%d+", i);
     }
-    printf("%d = %d\n", number, total);
+    printf("%d = %d\n", number, total); //i added the last number outside of the for loop
 }
 
